a5ef2fe2d8a648ec700aa4b59f0e2ba6b35e5aa5
Evgenia <kolontai.e.v@yandex.ru>
2022-06-03 12:25:05 +0300

    change class

--- program.java
+++ program.java
@@ -1,15 +1,14 @@
-class Solution {
+public class program {  
     public int a = 0;
-    public int b = 1;
-
-    public int ProductAndSum(int n) {
-        if (n > 0) {
+    public int b = 1; 
+    public int subtractProductAndSum(int n) {
+        while (n > 0) {
             int d = n % 10;
             a = a + d;
-            b = b * d;
-            n = n / 10;
-            ProductAndSum(n / 10);
+            b = b* d;
+            n=n/ 10;
+            subtractProductAndSum(n/10);
         }
-        return b - a;
+        return b-a;
     }
 }
